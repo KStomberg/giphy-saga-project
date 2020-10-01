b138c90474afc63c2de20ee4d26a0d9c74e835fd
John Riviere <mr.jriviere@gmail.com>
2020-10-01 10:57:35 -0500

    added favorites and search comps

---
+++ src/components/FavoritesView/FavoritesView.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+
+class FavoritesView extends Component {
+
+    render() {
+      return (
+        <div>
+          <h1>Favorites</h1>
+  
+  
+  
+        </div>
+      );
+    }
+    
+  }
+  
+  export default FavoritesView;
---
+++ src/components/SearchView/SearchView.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+
+
+class SearchView extends Component {
+
+  render() {
+    return (
+      <div>
+        <h1>Giphy Search!</h1>
+
+      <input placeholder="search input"></input><button>Search</button>
+
+
+
+      </div>
+    );
+  }
+  
+}
+
+export default SearchView;
