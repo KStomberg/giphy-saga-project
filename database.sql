e2ab468800432feaf4f3f461aa0ed4b31fe4f6d8
KStomberg <68118089+KStomberg@users.noreply.github.com>
2020-10-02 09:42:46 -0500

    working client side POST to favorite

--- database.sql
+++ database.sql
@@ -12,3 +12,9 @@ CREATE TABLE "category" (
 -- Default categories. You may change them :)
 INSERT INTO "category" ("name")
 VALUES ('funny'), ('cohort'), ('cartoon'), ('nsfw'), ('meme');
+
+CREATE TABLE "favorites" (
+    "id" SERIAL PRIMARY KEY,
+    "url" VARCHAR (256) NOT NULL,
+    "category_id" INT 
+);
